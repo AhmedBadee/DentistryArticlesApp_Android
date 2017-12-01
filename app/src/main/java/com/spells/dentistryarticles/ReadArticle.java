5f05cc9b881b746b31d27b82f0b3b126c9cea261
AhmedBadee <ahmedbadee17@gmail.com>
2017-12-01 22:18:28 +0200

    receives the aticle title and gets the body from the server

--- app/src/main/java/com/spells/dentistryarticles/ReadArticle.java
+++ app/src/main/java/com/spells/dentistryarticles/ReadArticle.java
@@ -1,13 +1,30 @@
 package com.spells.dentistryarticles;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class ReadArticle extends AppCompatActivity {
 
-    private TextView article;
+    private TextView articleTitleTxtView;
+    private TextView articleBodyTxtView;
     private String articleTitle;
 
     @Override
@@ -18,7 +35,86 @@ public class ReadArticle extends AppCompatActivity {
         Intent intent = getIntent();
         articleTitle = intent.getStringExtra("ArticleTitle");
 
-        article = (TextView) findViewById(R.id.article_title);
-        article.setText(articleTitle);
+        articleTitleTxtView = (TextView) findViewById(R.id.article_title);
+        articleTitleTxtView.setText(articleTitle);
+
+        articleBodyTxtView  = (TextView) findViewById(R.id.article_body);
+
+        new HTTPConnection(ReadArticle.this).execute();
+    }
+
+    private class HTTPConnection extends AsyncTask<Void, Void, JSONObject> {
+
+        private URL nodeUrl;
+        private HttpsURLConnection urlConnection;
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        private AlertDialog.Builder alertDialogBuilder;
+        private AlertDialog alertDialog;
+
+        private Context context;
+
+        private Bitmap[] image;
+
+        HTTPConnection(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setMessage("Loading...");
+            alertDialogBuilder.setCancelable(false);
+
+            alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... voids) {
+
+            JSONObject article = null;
+
+            try {
+                nodeUrl = new URL("https://dentistry.herokuapp.com/mobile/single/article/" + articleTitle);
+                urlConnection = (HttpsURLConnection) nodeUrl.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null)
+                    stringBuilder.append(line);
+
+                inputStreamReader.close();
+
+                article = new JSONObject(stringBuilder.toString());
+
+                /* JSONArray imagesURLs = article.getJSONArray("images_urls");
+
+                for (int i = 0; i < imagesURLs.length(); i++) {
+                    image[i] = BitmapFactory.decodeStream(new URL(imagesURLs.getString(i)).openStream());
+                } */
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
+            }
+            return article;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject article) {
+            try {
+                articleBodyTxtView.setText(article.getString("article"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            alertDialog.dismiss();
+        }
     }
 }
