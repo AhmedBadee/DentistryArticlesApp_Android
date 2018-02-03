873775e0459a717d43b8ab6ef9426b3516aca77c
AhmedBadee <ahmedbadee17@gmail.com>
2018-02-03 02:44:23 +0200

    added adview

--- app/src/main/java/com/spells/dentistryarticles/UserType.java
+++ app/src/main/java/com/spells/dentistryarticles/UserType.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class UserType extends AppCompatActivity {
 
     private boolean community_professional;
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_type);
+
+        adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 
     public void userTypeBtnClick(View view) {
@@ -29,6 +37,5 @@ public class UserType extends AppCompatActivity {
 
         intent.putExtra("UserType", community_professional);
         startActivity(intent);
-        finish();
     }
 }
